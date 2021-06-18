955f5a178d5021c02d21090d9012e3a8f04a098b
joaomsimoes <77450786+joaomsimoes@users.noreply.github.com>
2021-06-18 12:04:42 +0100

    almost there

--- predict_page.py
+++ predict_page.py
@@ -18,22 +18,15 @@ def show_predict_page():
 
     st.write('''We need some information to predict the car price''')
 
-    brands = ('alfa-romero', 'audi', 'bmw', 'chevrolet', 'dodge', 'honda',
-       'isuzu', 'jaguar', 'mazda', 'mercedes-benz', 'mercury',
-       'mitsubishi', 'nissan', 'peugot', 'plymouth', 'porsche', 'renault',
-       'saab', 'subaru', 'toyota', 'volkswagen', 'volvo')
-
-    aspirations = ('std', 'turbo')
-
-    brand = st.selectbox('Brand', brands)
-    aspiration = st.selectbox('Type', aspirations)
-
     enginesize = st.slider('engine-size', 60, 320)
     horsepower = st.slider('horsepower', 50, 260)
 
+    curb = st.slider('curb-weight', 1710, 4000)
+    citympg = st.slider('city-mpg', 15, 49)
+
     ok = st.button('Predict price')
     if ok:
-        X = np.array([[brands, aspirations, enginesize, horsepower]])
+        X = np.array([[enginesize, horsepower, curb, citympg]])
         X_final = full_pipeline.transform(X)
 
         price = model.predict(X_final)
